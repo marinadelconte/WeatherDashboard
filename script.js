7a2be9692bd375361a470c5061247dca81ce5b8c
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-06 19:48:29 -0400

    added get lat and lon function

--- script.js
+++ script.js
@@ -6,21 +6,26 @@ let city;
 
 
 
-fetch("https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=291f76a1b16ded2c803cb4e3fa8f978f")
-.then(response => response.json())
-.then(citiesFound => {
-    let firstCity = citiesFound[0];
+// fetch("https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=291f76a1b16ded2c803cb4e3fa8f978f")
+// .then(response => response.json())
+// .then(citiesFound => {
+//     let firstCity = citiesFound[0];
 
-    console.log(firstCity.lat);
-    console.log(firstCity.lon);
+//     console.log(firstCity.lat);
+//     console.log(firstCity.lon);
 
-    return fetch("")
-})
+//     return fetch("")
+// })
+// .then(response => response.json())
+// .then(data => {
 
+//     console.log(data);
+// })
 
+function get_lat_long(city_name, state_code, country_code) {
+    let cityFound = fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city_name+","+state_code+","+country_code+"&limit=1&appid=291f76a1b16ded2c803cb4e3fa8f978f");
+    console.log(cityFound);
+}
 
-.then(response => response.json())
-.then(data => {
+get_lat_long("Miami", "FL", "US");
 
-    console.log(data);
-})
