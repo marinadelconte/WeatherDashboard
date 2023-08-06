08be03b193beaff6ff41ea8fd040b52b5a388c8d
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-05 21:47:49 -0400

    added api key

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-
+let apiKey = "291f76a1b16ded2c803cb4e3fa8f978f";
+let city;
 
 ///add entire fetch into an event listener
 
-fetch("")
+
+
+
+fetch("https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=291f76a1b16ded2c803cb4e3fa8f978f")
 .then(response => response.json())
 .then(citiesFound => {
     let firstCity = citiesFound[0];
