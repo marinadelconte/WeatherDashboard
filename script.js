ba6bf6428405a7aef0cf38aff53ea178c710ea93
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-06 23:33:59 -0400

    added event listener

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 let apiKey = "291f76a1b16ded2c803cb4e3fa8f978f";
-let city;
-
-///add entire fetch into an event listener
+let fetchButton = document.querySelector(".btn");
 
 
+getCoordinates("Miami", "FL", "US");
 
 
-// fetch("https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=291f76a1b16ded2c803cb4e3fa8f978f")
-// .then(response => response.json())
-// .then(citiesFound => {
-//     let firstCity = citiesFound[0];
+function getCoordinates(city_name, state_code, country_code) {
 
-//     console.log(firstCity.lat);
-//     console.log(firstCity.lon);
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city_name+","+state_code+","+country_code+"&limit=1&appid="+apiKey)
+    .then(response => response.json())
+    .then(cityFound => {
+        let searchCity = cityFound[0];
+        let lat = searchCity.lat;
+        let lon = searchCity.lon;
+        return(getWeatherByCoordinates(lat, lon)) 
+    })
 
-//     return fetch("")
-// })
-// .then(response => response.json())
-// .then(data => {
-
-//     console.log(data);
-// })
-
-function get_lat_long(city_name, state_code, country_code) {
-    let cityFound = fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city_name+","+state_code+","+country_code+"&limit=1&appid=291f76a1b16ded2c803cb4e3fa8f978f");
-    console.log(cityFound);
 }
 
-get_lat_long("Miami", "FL", "US");
+function getWeatherByCoordinates(lat, lon) {
+    console.log(lat, lon)
+    fetch("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&appid=" + apiKey)
+    .then(response => response.json())
+    .then(data => {
 
+        console.log(data.list)
+        // let temp = weather.temp;
+        // let wind = weather[list.wind.speed];
+        // let humidity = weather[list.main.humidity];
+})
+}
+fetchButton.addEventListener('click', function() {
+
+})
