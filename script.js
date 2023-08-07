58e70b55ad4e8b7a734806b5b430e4b30df2e306
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-07 15:57:35 -0400

    added for loop to append content to each weather card

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let apiKey = "291f76a1b16ded2c803cb4e3fa8f978f";
 let fetchButton = document.querySelector(".btn");
+let cityName = document.querySelector("#cityName");
+let temp = document.querySelector("#temp");
+let humidity = document.querySelector("#humidity");
+let wind = document.querySelector("#wind");
+let search = document.querySelector("#search");
+let userInput = document.querySelector("#userInput");
+let rowElement = document.querySelector(".row");
 
 
-getCoordinates("Miami", "FL", "US");
-
-
-function getCoordinates(city_name, state_code, country_code) {
-
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city_name+","+state_code+","+country_code+"&limit=1&appid="+apiKey)
+function getCoordinates(event) {
+event.preventDefault();
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+userInput.value+"&limit=1&units=imperial&appid="+apiKey)
     .then(response => response.json())
     .then(cityFound => {
         let searchCity = cityFound[0];
@@ -18,46 +22,57 @@ function getCoordinates(city_name, state_code, country_code) {
 
 }
 
+function renderFiveDays(data){
+for (let i = 0; i < data.list.length; i= i+8) {
+    console.log(data.list[i]);
+const html= `   <div class="card col-2" style="width: 16rem; height: 17rem;">
+<div class="card-body">
+    <h5 class="card-title">Date: ${data.list[i].dt_txt}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Temp:</h6>
+    <p class="card-text">Humidity:</p>
+    <p class="card-text">Wind Speed:</p>
+</div>
+</div>`   
+rowElement.insertAdjacentHTML("beforeend", html);
+}
+}
+
+
+
 function getWeatherByCoordinates(lat, lon) {
     console.log(lat, lon)
-    fetch("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&appid=" + apiKey)
+    fetch("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey)
     .then(response => response.json())
     .then(data => {
 
+    console.log(data);
+    renderFiveDays(data);
         //first card
-        console.log(data.list[0])
+        console.log(data.list[0].main.temp)
+        console.log(data.list[0].main.humidity)
+        console.log(data.list[0].wind.speed)
+
+        temp.textContent = `Temperature: ${data.list[0].main.temp} degrees`
+
         
-
-        console.log(`  <div class="card" style="width: 16rem; height: 17rem;">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted"></h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-        </div>
-    </div>`)
-
-
         //second card
-        console.log(data.list[8])
+        //console.log(data.list[8])
 
         //third card
-        console.log(data.list[16])
+        //console.log(data.list[16])
 
         //fourth card
-        console.log(data.list[24])
+        //console.log(data.list[24])
 
         //fifth card
-        console.log(data.list[32])
+       //console.log(data.list[32])
 
         //sixth card 
-        console.log(data.list[40])
+       //console.log(data.list[39])
 
         
       
 })
 }
-fetchButton.addEventListener('click', function(getCoordinates) {
-getCoordinates.preventDefault();
-
-})
+search.addEventListener('click', getCoordinates) 
+console.log(userInput.value);
