3d9fdfb5ecfde0ff1dabe40f73ddb47565d9ee24
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-07 23:34:16 -0400

    added images to weather cards

--- script.js
+++ script.js
@@ -7,72 +7,76 @@ let wind = document.querySelector("#wind");
 let search = document.querySelector("#search");
 let userInput = document.querySelector("#userInput");
 let rowElement = document.querySelector(".row");
+let columnElement = document.querySelector(".column");
 
 
 function getCoordinates(event) {
-event.preventDefault();
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+userInput.value+"&limit=1&units=imperial&appid="+apiKey)
-    .then(response => response.json())
-    .then(cityFound => {
-        let searchCity = cityFound[0];
-        let lat = searchCity.lat;
-        let lon = searchCity.lon;
-        return(getWeatherByCoordinates(lat, lon)) 
-    })
+    event.preventDefault();
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput.value + "&limit=1&units=imperial&appid=" + apiKey)
+        .then(response => response.json())
+        .then(cityFound => {
+            let searchCity = cityFound[0];
+            let lat = searchCity.lat;
+            let lon = searchCity.lon;
+            return (getWeatherByCoordinates(lat, lon))
+        })
 
 }
 
-function renderFiveDays(data){
-for (let i = 0; i < data.list.length; i= i+8) {
-    console.log(data.list[i]);
-const html= `   <div class="card col-2" style="width: 16rem; height: 17rem;">
+function renderCurrentDay(data) {
+    const html = `   <div class="card col-6" style="width: 32rem; height: 20rem;">
 <div class="card-body">
-    <h5 class="card-title">Date: ${data.list[i].dt_txt}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Temp:</h6>
-    <p class="card-text">Humidity:</p>
-    <p class="card-text">Wind Speed:</p>
+        <h2 class="card-title">City: ${userInput.value}</h2>
+        <h6 class="card-title">Date: ${dayjs().format("MM/DD/YYYY")}</h6>
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+        <p class="card-title">Temp: ${data.main.temp}°F</p>
+        <p class="card-text">Humidity: ${data.main.humidity}%</p>
+        <p class="card-text">Wind Speed: ${data.wind.speed} mph</p>
 </div>
-</div>`   
-rowElement.insertAdjacentHTML("beforeend", html);
-}
+</div>`
+    columnElement.insertAdjacentHTML("beforeend", html);
 }
 
+
+
+function renderFiveDays(data) {
+    for (let i = 0; i < data.list.length; i += 8) {
+        console.log(data.list[i]);
+        const html = `   <div class="card col-2" style="width: 16rem; height: 17rem;">
+<div class="card-body">
+        <h6 class="card-title">Date: ${data.list[i].dt_txt}</h6>
+        <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png"/>
+        <p class="card-title">Temp: ${data.list[i].main.temp}°F</p>
+        <p class="card-text">Humidity: ${data.list[i].main.humidity}%</p>
+        <p class="card-text">Wind Speed: ${data.list[i].wind.speed} mph</p>
+</div>
+</div>`
+        rowElement.insertAdjacentHTML("beforeend", html);
+    }
+}
 
 
 function getWeatherByCoordinates(lat, lon) {
     console.log(lat, lon)
-    fetch("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey)
-    .then(response => response.json())
-    .then(data => {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey)
+        .then(response => response.json())
+        .then(data => {
 
-    console.log(data);
-    renderFiveDays(data);
-        //first card
-        console.log(data.list[0].main.temp)
-        console.log(data.list[0].main.humidity)
-        console.log(data.list[0].wind.speed)
-
-        temp.textContent = `Temperature: ${data.list[0].main.temp} degrees`
-
-        
-        //second card
-        //console.log(data.list[8])
-
-        //third card
-        //console.log(data.list[16])
-
-        //fourth card
-        //console.log(data.list[24])
-
-        //fifth card
-       //console.log(data.list[32])
-
-        //sixth card 
-       //console.log(data.list[39])
-
-        
-      
-})
+            renderFiveDays(data);
+            getCurrentWeather(lat, lon);
+        })
 }
-search.addEventListener('click', getCoordinates) 
+search.addEventListener('click', getCoordinates)
 console.log(userInput.value);
+
+function getCurrentWeather(lat, lon) {
+    fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey)
+        .then(response => response.json())
+        .then(data => {
+
+            renderCurrentDay(data);
+
+        })
+}
+
+
